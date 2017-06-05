b3ca296ee7c2fbd5969c9ef9e162545fb8b4a0ae
Sunmughan Swamy <sunmughaN@Gmail.com>
2017-06-05 10:58:52 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/saputra/facedetection/MainActivity.java
+++ app/src/main/java/com/example/saputra/facedetection/MainActivity.java
@@ -10,37 +10,30 @@ import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
-
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
 import com.google.android.gms.vision.face.LargestFaceFocusingProcessor;
 
 import java.io.IOException;
-
 public class MainActivity extends AppCompatActivity {
-
     SurfaceView surfaceView;
     CameraSource mCameraSource;
     TextView textView;
     FaceDetector faceDetector;
     public FaceView mView;
     final int RequestCameraPermisson = 1001;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         surfaceView = (SurfaceView) findViewById(R.id.surface);
         textView = (TextView) findViewById(R.id.text);
         mView = (FaceView) findViewById(R.id.view1);
-
         faceDetector = new FaceDetector.Builder(getApplicationContext())
                 .setProminentFaceOnly(true)
                 .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
                 .build();
-
         faceDetector.setProcessor(
                 new LargestFaceFocusingProcessor.Builder(faceDetector, new FaceTracker(this))
                         .build());
@@ -51,19 +44,16 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         startCameraSource();
     }
-
     @Override
     protected void onPause() {
         super.onPause();
         mCameraSource.stop();
     }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
         mCameraSource.release();
     }
-
     private void startCameraSource() {
         if (!faceDetector.isOperational()) {
             Log.d("xxx", "heemmm");
@@ -74,11 +64,9 @@ public class MainActivity extends AppCompatActivity {
                     .setRequestedFps(2.0f)
                     .setAutoFocusEnabled(true)
                     .build();
-
             surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
                 @Override
                 public void surfaceCreated(SurfaceHolder holder) {
-
                     try {
                         if (ActivityCompat.checkSelfPermission(getApplicationContext(),
                                 Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
@@ -87,28 +75,21 @@ public class MainActivity extends AppCompatActivity {
                                     RequestCameraPermisson);
                         }
                         mCameraSource.start(surfaceView.getHolder());
-
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-
                 }
-
                 @Override
                 public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
                 }
-
                 @Override
                 public void surfaceDestroyed(SurfaceHolder holder) {
 
                     mCameraSource.stop();
-
                 }
             });
         }
     }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
@@ -122,19 +103,15 @@ public class MainActivity extends AppCompatActivity {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-
                 }
             }
         }
     }
-
     public void drawCircleOnFace(final Face face){
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-
                 mView.setContent(face);
-
             }
         });
     }
